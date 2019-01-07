8d2284e65190f33030c97533d3d9d417ec7fee22
jaredgreen <jgreen2@pillartechnology.com>
2019-01-07 14:48:09 -0600

    Allow using import within step defintions. Broke preset react-app

--- features/step_definitions/fizzbuzz.steps.js
+++ features/step_definitions/fizzbuzz.steps.js
@@ -1,6 +1,15 @@
-const { Given, When, Then } = require('cucumber')
+// import App from "../../src/App";
+// import React from "react";
+// import {mount} from "enzyme";
+import {Given, When, Then} from 'cucumber';
+// const App = require('../../src/App')
+// const React = require('react');
+// const { mount } = require('enzyme');
+// const { Given, When, Then } = require('cucumber');
 
 Given(/Input contains (\d+)/, function(input) {
+    // const app = mount(<App />);
+    // app.find('#input').simulate('change', {target: {value: input}});
     console.log(input);
 });
 
